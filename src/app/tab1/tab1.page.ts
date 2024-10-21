ceddbaab9a662d02e798c4fef8c522c24975a9e9
lenintoto <lenincgg_aries1@hotmail.com>
2024-10-21 17:35:44 -0500

    practica Examen

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,54 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
-
+    resultado: string = '0';
+    operador: string | null = null;
+    primerOperando: number | null = null;
+  
+    agregarInput(valor: string) {
+      if (this.resultado === '0') {
+        this.resultado = valor;
+      } else {
+        this.resultado += valor;
+      }
+    }
+  
+    agregarOperador(op: string) {
+      if (this.primerOperando === null) {
+        this.primerOperando = parseFloat(this.resultado);
+        this.operador = op;
+        this.resultado = '0';
+      } else {
+        this.calcular();
+        this.operador = op;
+      }
+    }
+  
+    calcular() {
+      if (this.primerOperando !== null && this.operador) {
+        const segundoOperando = parseFloat(this.resultado);
+        switch (this.operador) {
+          case '+':
+            this.resultado = (this.primerOperando + segundoOperando).toString();
+            break;
+          case '-':
+            this.resultado = (this.primerOperando - segundoOperando).toString();
+            break;
+          case '*':
+            this.resultado = (this.primerOperando * segundoOperando).toString();
+            break;
+          case '/':
+            this.resultado = (this.primerOperando / segundoOperando).toString();
+            break;
+        }
+        this.primerOperando = null;
+        this.operador = null;
+      }
+    }
+  
+    reset() {
+      this.resultado = '0';
+      this.primerOperando = null;
+      this.operador = null;
+    }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { addIcons } from 'ionicons';
+import { logoFacebook, logoGithub, logoInstagram } from 'ionicons/icons';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor() {
+    addIcons({logoFacebook});
+    addIcons({logoGithub});
+    addIcons({logoInstagram});
 
+  }
 }
